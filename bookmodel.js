6a4692ebf55549ba168d8126df61a3ee046921f1
rushabhjsoni <slayer.r1@gmail.com>
2021-11-11 05:08:41 +0530

    completed server

--- bookmodel.js
+++ bookmodel.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
   title:String,
   description: String,
-  status: Boolean,
+  status: String,
   email : String
 });
 
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// import our library that we are going to use to interact (make queries and stuff) with our mongo db
 const mongoose = require('mongoose');
 const Book = require ('./bookmodel.js');
 const app = express();
 app.use(cors());
-
-
+app.use(express.json())
 
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
@@ -15,9 +13,13 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('Mongoose is connected')
 });
+
 const PORT = process.env.PORT
+
 app.get('/', (req, res) => res.send('hello'));
 app.get('/books', handleBooks);
+app.delete('/books/:id', handleDeleteBooks);
+app.post('/books', handlePostBooks);
 
 async function handleBooks (req,res){
 
@@ -28,12 +30,52 @@ try {
   res.status(200).send(booksFromDB);
   } else {
     res.status(404).send('no books for you');
+
+  const email = req.query.email;
+  try {
+    let booksFromDB = await Book.find({email: email});
+    if (booksFromDB) {
+    res.status(200).send(booksFromDB);
+    } else {
+      res.status(404).send('no books for you');
+    }
+    } catch (e) {
+      console.error(e);
+      res.status(500).send('server error')
+    }
   }
 
-} catch (e) {
-  console.error(e);
-  res.status(500).send('server error')
+async function handlePostBooks(req,res) {
+
+  const newBook = { ...req.body, email: req.query.email}
+  try {
+    let successBook = await Book.create(newBook);
+    if (successBook) {
+      res.status(200).send(successBook);
+    } else {
+      res.status(404).send('no books for you');
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('server error')
+  }
 }
+
+async function handleDeleteBooks(req,res){
+  const id = req.params.id
+  const email = req.query.email;
+  try {
+    const book = await Book.findOne({_id: id, email: email}); 
+    if (!book) {
+     res.status(400).send('No have');
+    } else {
+      await Book.findByIdAndDelete(id);
+      res.status(200).send('Deleted Book')
+    }
+    } catch (e) {
+    console.error(e);
+    res.status(500).send('server error')
+  }
 }
 
 
