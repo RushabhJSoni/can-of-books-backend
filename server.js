e17d2aa3f2bbd029d924936272a03ae6322a7998
rushabhjsoni <slayer.r1@gmail.com>
2021-11-11 18:50:31 +0530

    added readme data

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-mongoose.connect(process.env.REACT_APP_SERVER_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
